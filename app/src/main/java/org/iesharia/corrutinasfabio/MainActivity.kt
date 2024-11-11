083470f11faa8a62613d4ada51b9087e4e68ec4f
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-11 12:04:21 +0000

    Eliminamos errores

--- app/src/main/java/org/iesharia/corrutinasfabio/MainActivity.kt
+++ app/src/main/java/org/iesharia/corrutinasfabio/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -40,7 +41,7 @@ class MainActivity : ComponentActivity() {
 fun Pantalla() {
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
-    val contador: MutableState<Int> = remember { androidx.compose.runtime.mutableStateOf(6) }
+    val contador: MutableState<Int> = remember { mutableStateOf(6) }
 
     Column(
         modifier = Modifier
